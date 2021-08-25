8224ec3719a956aca2164d9def4461c00d45bb0d
ruth5555 <89256457+ruth5555@users.noreply.github.com>
2021-08-25 11:56:06 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+function preload(){
+seaImg=loadImage("sea.png");
+shipImg1=loadAnimation(//list the names of png files that you want to load);
+}
+
+function setup(){
+  createCanvas(400,400);
+ spriteName.
+  Dreamstime.com242 Alone Boat Middle Sea Photos - Free & Royalty-Free Stock Photos from Dreamstime(seaImg);
+}
+
+function draw() {
+  background("blue");
+ //code to reset the background
+ if(sea.x<0){
+   sea.x=sea.width/2
+ }
+}
